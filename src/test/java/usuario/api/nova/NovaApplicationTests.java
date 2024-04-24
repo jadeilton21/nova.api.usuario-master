472b6f468d4002f1ec6f33b5cd4e241aa337110e
jadei <jadeiltondm@gmail.com>
2024-04-24 17:09:07 -0300

    feat

---
+++ src/test/java/usuario/api/nova/NovaApplicationTests.java
@@ -0,0 +1,13 @@
+package usuario.api.nova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
