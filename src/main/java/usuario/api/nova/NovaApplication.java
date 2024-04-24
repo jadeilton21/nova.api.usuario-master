8a05773017e3f3b4734ea4985caadbad079860ff
jadei <jadeiltondm@gmail.com>
2024-04-24 17:09:00 -0300

    feat

---
+++ src/main/java/usuario/api/nova/NovaApplication.java
@@ -0,0 +1,13 @@
+package usuario.api.nova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaApplication.class, args);
+	}
+
+}
