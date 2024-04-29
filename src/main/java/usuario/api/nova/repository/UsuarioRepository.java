9cb5718ddb6d1c41d5ecbcbbeae617c78101153f
jadei <jadeiltondm@gmail.com>
2024-04-29 18:24:27 -0300

    feat: UsuarioRepository completed

--- src/main/java/usuario/api/nova/repository/UsuarioRepository.java
+++ src/main/java/usuario/api/nova/repository/UsuarioRepository.java
@@ -4,6 +4,7 @@ import jdk.jfr.Registered;
 import org.springframework.stereotype.Repository;
 import usuario.api.nova.doMain.Usuario;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,11 +22,20 @@ public class UsuarioRepository {
     }
 
     public void deletar(Integer id) {
+        System.out.println(String.format("DELETAR/ID - Deletando o id: %d e excluido o usuario", id));
+        System.out.println(id);
     }
 
     public List<Usuario> listar() {
+        System.out.println("LIST - Listando os usários do sistema");
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("jadeilton","password"));
+        usuarios.add(new Usuario("jade","masterpass"));
+        return usuarios;
     }
 
-    public Usuario testarUmUsuario() {
+    public Usuario testarUmUsuario(Integer id) {
+        System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
+        return new Usuario("jade","password");
     }
 }
