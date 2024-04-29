091855e1709a68ae89ab9ff9bb687204cb680efa
jadei <jadeiltondm@gmail.com>
2024-04-29 16:17:13 -0300

    feat: Create cadastrar

--- src/main/java/usuario/api/nova/repository/UsuarioRepository.java
+++ src/main/java/usuario/api/nova/repository/UsuarioRepository.java
@@ -2,7 +2,12 @@ package usuario.api.nova.repository;
 
 import jdk.jfr.Registered;
 import org.springframework.stereotype.Repository;
+import usuario.api.nova.doMain.Usuario;
 
 @Repository
 public class UsuarioRepository {
+
+
+    public void cadastrar(Usuario usuario) {
+    }
 }
