e4be51079b1051de2fa17286c95c193ed61bf617
jadei <jadeiltondm@gmail.com>
2024-04-29 15:35:50 -0300

    feat: Create UsuarioRepository

--- src/main/java/usuario/api/nova/controllers/UsuarioControllers.java
+++ src/main/java/usuario/api/nova/controllers/UsuarioControllers.java
@@ -3,6 +3,7 @@ package usuario.api.nova.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import usuario.api.nova.repository.UsuarioRepository;
 
 @RestController
 public class UsuarioControllers {
