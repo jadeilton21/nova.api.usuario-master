37d5ed2e9479a86a5ffaf5c1eb77bea46ef15513
jadei <jadeiltondm@gmail.com>
2024-04-29 16:10:31 -0300

    feat: Create crud post, put, delete

--- src/main/java/usuario/api/nova/controllers/UsuarioControllers.java
+++ src/main/java/usuario/api/nova/controllers/UsuarioControllers.java
@@ -2,7 +2,8 @@ package usuario.api.nova.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import usuario.api.nova.doMain.Usuario;
 import usuario.api.nova.repository.UsuarioRepository;
 
 @RestController
@@ -10,5 +11,18 @@ public class UsuarioControllers {
 
     @Autowired
     private UsuarioRepository repository;
+
+    @PostMapping
+    public void cadastrarUsuario(@RequestBody Usuario usuario){
+        repository.cadastrar(usuario);
+    }
+    @PutMapping
+    public void atualizarUsuario(@RequestBody Usuario usuario){
+        repository.atualizar(usuario);
+    }
+    @DeleteMapping("/usuario/{id}")
+    public void detelarUsuario(@PathVariable ("id") Integer id){
+        repository.deletar(id);
+    }
 
 }
