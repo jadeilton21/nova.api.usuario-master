172d7df7620a7b352799c1d9590cfe449f4aa7f4
jadei <jadeiltondm@gmail.com>
2024-04-30 13:33:42 -0300

    feat: create method Contact in SwaggerConfiguracoes

--- src/main/java/usuario/api/nova/doc/SwaggerConfiguracoes.java
+++ src/main/java/usuario/api/nova/doc/SwaggerConfiguracoes.java
@@ -1,9 +1,18 @@
 package usuario.api.nova.doc;
 
 
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.Contact;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
 public class SwaggerConfiguracoes {
+
+    private Contact contato() {
+        return new Contact(
+                "jadeilton",
+                "http://www.jaderasta.com.br",
+                "jade@rasta.com.br");
+    }
 }
