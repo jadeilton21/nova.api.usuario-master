46e3dbae15ac9f8ce20b10eb0187104a02415441
jadei <jadeiltondm@gmail.com>
2024-04-30 13:41:25 -0300

    feat: create method detalheApi in SwaggerConfiguracoes

--- src/main/java/usuario/api/nova/doc/SwaggerConfiguracoes.java
+++ src/main/java/usuario/api/nova/doc/SwaggerConfiguracoes.java
@@ -1,10 +1,18 @@
 package usuario.api.nova.doc;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 @EnableSwagger2
 @Configuration
@@ -31,4 +39,21 @@ public class SwaggerConfiguracoes {
         return apiInfoBuilder;
 
     }
+    @Bean
+    public Docket detalheApi() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+
+        docket
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("web.dio.api.my.dio.api.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.informacoesApi().build())
+                .consumes(new HashSet<String>(Arrays.asList("application/json")))
+                .produces(new HashSet<String>(Arrays.asList("application/json")));
+
+        return docket;
+    }
+
+
 }
